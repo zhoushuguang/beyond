0cb4a1cb6d0fee5f3af6eb79a57752334ef5840e
wyzhangyuhan <1063883180@qq.com>
2023-10-07 20:08:15 +0800

    fix: register logic

--- application/applet/internal/code/code.go
+++ application/applet/internal/code/code.go
@@ -4,7 +4,9 @@ import "beyond/pkg/xcode"
 
 var (
 	RegisterMobileEmpty   = xcode.New(10001, "注册手机号不能为空")
+	RegisterMobileEmpty   = xcode.New(10001, "密码不能为空")
 	VerificationCodeEmpty = xcode.New(100002, "验证码不能为空")
 	MobileHasRegistered   = xcode.New(100003, "手机号已经注册")
 	LoginMobileEmpty      = xcode.New(100003, "手机号不能为空")
+	RegisterPasswdEmpty   = xcode.New(100004, "密码不能为空")
 )
