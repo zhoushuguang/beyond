58209c68a7ba03831b0abc46e07ff7c109d23c38
cpf2021-gif <2992247892@qq.com>
2023-10-17 12:14:49 +0800

    fix: Resolve data disorder issue after in-process data batching with MapReduce

--- application/article/rpc/internal/logic/articleslogic.go
+++ application/article/rpc/internal/logic/articleslogic.go
@@ -1,8 +1,10 @@
 package logic
 
 import (
+	"cmp"
 	"context"
 	"fmt"
+	"slices"
 	"strconv"
 	"time"
 
@@ -84,6 +86,20 @@ func (l *ArticlesLogic) Articles(in *pb.ArticlesRequest) (*pb.ArticlesResponse, 
 		if err != nil {
 			return nil, err
 		}
+
+		// 通过sortFiled对articles进行排序
+		var cmpFunc func(a, b *model.Article) int
+		if sortField == "like_num" {
+			cmpFunc = func(a, b *model.Article) int {
+				return cmp.Compare(b.LikeNum, a.LikeNum)
+			}
+		} else {
+			cmpFunc = func(a, b *model.Article) int {
+				return cmp.Compare(b.PublishTime.Unix(), a.PublishTime.Unix())
+			}
+		}
+		slices.SortFunc(articles, cmpFunc)
+
 		for _, article := range articles {
 			curPage = append(curPage, &pb.ArticleItem{
 				Id:           article.Id,
