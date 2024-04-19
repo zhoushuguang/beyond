89245202e203711ad1d82c506e47dcb3afd3b367
zhouweijie <zhouweijie@192.168.5.2>
2024-04-19 22:31:43 +0800

    :bug: fix update time field of reply sql file

--- db/reply.sql
+++ db/reply.sql
@@ -8,7 +8,7 @@ CREATE TABLE `reply_count` (
     `reply_num` int(11) NOT NULL DEFAULT '0' COMMENT '评论总数',
     `reply_root_num` int(11) NOT NULL DEFAULT '0' COMMENT '根评论总数',
     `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_mtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
     PRIMARY KEY (`id`),
     KEY `ix_update_time` (`update_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='评论计数';
@@ -24,7 +24,7 @@ CREATE TABLE `reply` (
     `status` tinyint(4) NOT NULL DEFAULT '0' COMMENT '状态 0:正常 1:删除',
     `like_num` int(11) NOT NULL DEFAULT '0' COMMENT '点赞数',
     `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `update_mtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
+    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
     PRIMARY KEY (`id`),
     KEY `ix_update_time` (`update_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='评论表';
